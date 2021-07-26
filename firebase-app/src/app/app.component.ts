d07c330a292e71b1cc58f32669c8bad6832d0ac2
Henry Worrall <61287647+henry-worrall@users.noreply.github.com>
2021-07-26 17:53:04 +0100

    create and loop through tasks

--- firebase-app/src/app/app.component.ts
+++ firebase-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-app';
+  todo: Task[] = [
+    {
+      title: 'Be awesome!',
+      description: 'Go out and be awesome'
+    },
+    {
+      title: 'Create an Angular Firebase app',
+      description: 'Using Firebase and Angular create a app!'
+    }
+  ];
 }
